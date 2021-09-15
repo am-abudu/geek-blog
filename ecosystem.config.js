44d68f1d52691a041aee8d4a0cb65d59cad59df6
Abudu <abudulin@foxmail.com>
2021-09-15 23:55:07 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,15 @@ module.exports = {
         args: 'start',
         env: {
             NODE_ENV: "production",
-            PORT: $PORT
+            PORT: 3095
         }
-    }]
+    }],
+	server: {
+    // 默认localhost 只能本地访问
+    // 若想要对外访问，要配置为 0.0.0.0
+    host: '0.0.0.0',
+    // nuxtjs 默认 3000
+    // vercel 默认80
+    port: 80
+  }
 }
